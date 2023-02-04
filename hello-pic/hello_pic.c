0e99e56ec0ab33cb386da10199db6ccc59a2b6e9
Zou Hanya <hanyazou@gmail.com>
2023-02-04 20:35:32 +0900

    Add SPI controller functions

--- hello-pic/hello_pic.c
+++ hello-pic/hello_pic.c
@@ -80,6 +80,7 @@
 
 #include <xc.h>
 #include <stdio.h>
+#include "SPI.h"
 
 #define Z80_CLK 2500000UL	// Z80 clock frequency(Max 16MHz)
 
@@ -96,6 +97,8 @@
 #define UART_CREG 0xE001	// Control REG
 
 #define _XTAL_FREQ 64000000UL
+#define SPI_CLOCK_100KHZ 10     // Determined by actual measurement
+#define SPI_CLOCK_2MHZ 0        // Maximum speed w/o any wait (1~2 MHz)
 
 // 0xf000-0xf003 hex disp
 //
@@ -355,6 +358,56 @@ void __interrupt(irq(CLC1),base(8)) CLC_ISR(){
     G3POL = 0;
 }
 
+void SPI_test(void)
+{
+	uint8_t buf[16];
+	printf("\n\rtest SPI ...\n\r");
+	SPI_begin();
+
+	SPI_configure(SPI_CLOCK_100KHZ, SPI_MSBFIRST, SPI_MODE0);
+	SPI_begin_transaction();
+	SPI_dummy_clocks(10);
+	SPI_end_transaction();
+
+	// CMD0
+	SPI_begin_transaction();
+	SPI_dummy_clocks(1);
+	buf[0] = 0x40;  // CMD0
+	buf[1] = 0x00;
+	buf[2] = 0x00;
+	buf[3] = 0x00;
+	buf[4] = 0x00;
+	buf[5] = 0x95;  // CRC
+	SPI_send(buf, 6);
+	SPI_dummy_clocks(1);
+	buf[0] = SPI_receive_byte();
+	SPI_end_transaction();
+	printf("send CMD0, R1=%02x\n\r", buf[0]);
+
+	SPI_configure(SPI_CLOCK_2MHZ, SPI_MSBFIRST, SPI_MODE0);
+
+	// CMD8
+	SPI_begin_transaction();
+	SPI_dummy_clocks(1);
+	buf[0] = 0x48;  // CMD8
+	buf[1] = 0x00;
+	buf[2] = 0x00;
+	buf[3] = 0x01;
+	buf[4] = 0xaa;
+	buf[5] = 0x87;  // CRC
+	SPI_send(buf, 6);
+	SPI_dummy_clocks(1);
+	buf[0] = SPI_receive_byte();
+	if (buf[0] == 0x01) {
+		SPI_receive(&buf[1], 4);
+	} else {
+		SPI_memset(&buf[1], 0x00, 4);
+	}
+	SPI_end_transaction();
+	printf("send CMD8, R7=%02x %02x %02x %02x %02x\n\r",
+	       buf[0], buf[1], buf[2], buf[3], buf[4]);
+}
+
 // main routine
 void main(void) {
 	int i;
@@ -533,6 +586,9 @@ void main(void) {
     // Z80 start
     GIE = 1;		// Global interrupt enable
 
+#if 1
+    SPI_test();
+#else
 	ledwrite(0x0F, 0x00);	// display test , normal operation
 	ledwrite(0x0A, 0x00);	// Intensity , Duty Cycle 0x00=1/32 .. 0x0F=31/32
 	ledwrite(0x0B, 0x07);	// scan limit , display 0 to 7
@@ -565,6 +621,7 @@ void main(void) {
 	    __delay_ms(1000);
 	    LATE2 = 1;
     }
+#endif
 
     while(1);
 };
